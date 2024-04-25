e60b46413b4023f01e71b865b6013a16e5fc4260
= <aymen.moussaoui@be.ey.com>
2024-04-25 10:47:56 +0200

    Last changes + ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# hackathon-timesheet
+# Tech Club - Hackathon - Worst Timesheet
+<div style="text-align:center;">
+<img src="./images/hackathon.PNG" alt="Hackathon" width="300">
+</div>
+
+Welcome to T&I's Hackathon! We're thrilled to have you with us. This hackathon is an opportunity for you to showcase your skills and creativity by building the worst timesheet experience within a limited time frame.
+
+This repository contains all resources and guidelines for participating in the hackathon. Here you'll find everything you need to get started, including setup instructions, rules, and useful links to aid you in your coding journey.
+
+## Table Of Content 
+- [Get Started](#getstarted)
+    - [VM Configuration](#vm)
+    - [Local PC Configuration](#pc)
+- [Rules](#rules)
+- [Evaluation](#evaluation)
+- [Useful Links](#links)
+
+# Get Started <a name="getstarted"></a>
+First off, with your team, decide the basis of your website:
+- *Do you want to use your prefered programming languages and technologies?* You can write the code from scrath with no limitations!
+- *Do you prefer to start with a functioning and familiar timesheet website, shown by the image below?* You can use the source code of this repository that is based on Angular (a NodeJS library).
+
+![Website](/images/timesheet_basis.PNG)
+
+Then, choose either one of the following workspaces for your team:
+- [Local PC Configuration](#pc) if you wish to code on your local PC, or
+- [VM Configuration](#vm) if you wish to be spared from the burden of EY-restricted PCs.
+
+## Local-PC Configuration <a name="pc"></a>
+So, you wish to code on your EY-restricted local PC? Very brave of you... You will need the following: 
+- **Code Editor**: Make sure you have a proper code editor. We recommend [Visual Studio Code](https://code.visualstudio.com/download).
+- If you prefer to start from this source code:
+  1. **Source Code**: Download this [ZIP-compressed source code](https://github.com/AymenMEY/hackathon-timesheet/archive/refs/heads/main.zip) and extract it locally.
+  2. **NodeJS**: Download [NodeJS](https://nodejs.org/en/download) and install it.
+  3. **Compile & Execute**:
+     1. Open the extracted folder with your code editor.
+     2. Compile and install all dependencies: ```npm install```
+     3. Execute the code: ```npm start```
+     4. You can now access the website via this address: http://localhost:4200/
+
+
+## VM Configuration <a name="vm"></a>
+So, you chose the easy way out... Smart of you. There are preconfigured machines that you can easily access. Just ask one of the organizers to give you access. You will be all set!
+
+# Rules <a name="rules"></a>
+>*"A timesheet is a method for recording the amount of a worker's time spent on each job." ~ Wikipedia.*
+- **Goal:** Build a functioning website with the worst timesheet experience.
+- **Free as a bird:** You can build your website either from the source code of this repo or start from scrath!
+- **The Only Requirement**: We let you interpret the definition cited above ... **but**, by the end, your timesheet must contain at least these details:
+  - An Engagement Code
+  - A Location
+  - Daily Work Hours  
+- **Code of Conduct**: Respect fellow participants and adhere to the hackathon's code of conduct. Discriminatory or offensive behavior will be not tolerated.
+
+> [!IMPORTANT]  
+> **Code Of Conduct** may be neglicted upon agreement with one of the organizers.
+
+
+# Evaluation <a name="evaluation"></a>
+Just like Eurovision, websites will be judged by every participant based on creativity and technical complexity:
+1. Each team presents their result in a few minutes.
+2. Each participant must give 1, 2, 3, 4, or 5 points to each team. Beware a point can only be given once. 
+3. If you feel generous enough, you can even give a +1 for the teams who touched your heart or whose code seems extra innovative. 
+4. Winners get the reward of their lives!
+
+## Useful Links <a name="links"></a>
+Here are some useful links to help you during the hackathon:
+
+- [Node.js Documentation](https://nodejs.org/en/docs/)
+- [GPT](https://chat.openai.com/)
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,120 +29,54 @@ export class AppComponent {
   weekDays = ['Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr'];
   getWeek = getWeek;
   selectedDate: Date = new Date();
-  selectedDays: any[] = [];
+  selectedWeek: any[] = [];
   timesheet: any[] = [];
-  timesheet_submitted: boolean = false;
+  timesheetSubmitted: boolean = false;
+  timesheetError: boolean = false;
 
   constructor() {
     this.febDays = this.getCalendarDays(2024, 1);
     this.marchDays = this.getCalendarDays(2024, 2);
     this.aprilDays = this.getCalendarDays(2024, 3);
-    this.getCurrentWeek();
-    this.initiateTimesheet();
+    this.updateSelectedWeek();
+    this.initTimesheet();
   }
 
-  initiateTimesheet() {
-    for(let i = 0; i< 5; i++) {
-      this.timesheet.push({
-        engId: undefined,
-        actId: undefined,
-        role: undefined,
-        workLoc: undefined, 
-        sat: undefined,
-        sun: undefined,
-        mon: undefined,
-        tue: undefined,
-        wed: undefined,
-        thu: undefined,
-        fri: undefined
-      })
-    }
-  }
+  
 
+  /**
+   * Handles timesheet submission and logs the current timesheet
+   */
   submitTimesheet() {
-    this.timesheet_submitted = true;
-    console.log(this.timesheet)
-  }
-
-  getDateStatus(date: Date, month: number) {
-    let isToday = '';
-    if (date.getMonth() != month) {
-      return isToday + 'disabled'
+    if (this.isTimesheetCorrect()) {
+      this.timesheetError = false;
+      this.timesheetSubmitted = true;
+    } else {
+      this.timesheetError = true;
+      this.timesheetSubmitted = false;
     }
-    if (date.getDay() === 6 || date.getDay() === 0) {
-      return isToday + 'nonworking'
+    console.log(this.timesheet);
+  }
+
+  isTimesheetCorrect() {
+    function containsUndefinedInArray(arr) {
+      // Returns true if any object in the array has any property that is undefined
+      console.log(arr);
+      return arr.some(obj => Object.values(obj).some(value => value === undefined));
     }
-    if (date.getDate() === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
-      isToday = 'today ';
-    }
+    return !containsUndefinedInArray(this.timesheet);
 
-    if (this.isInCurrentWeek(date)) {
-      return isToday + 'current';
-    }
-
-    let dayFound = this.businessDaysData.days.filter((d: any) => {
-      const d1 = parse(d.date, "dd/MM/yyyy", new Date());
-      return  d1.getDate() === date.getDate() && d1.getMonth() == date.getMonth();
-    })[0];
-
-    return dayFound && dayFound.status ? isToday + dayFound.status.toLowerCase() : isToday + 'default'
   }
 
-  selectDate(date:Date) {
-    this.selectedDate = date;
-    this.getCurrentWeek();
-  }
 
-  getCurrentWeek() {
-    let currentDay = this.selectedDate.getDay();
-    let startDay = addDays(this.selectedDate, -1*(currentDay+1));
-    this.selectedDays = [];
-    for (let i = 0; i < 7; i++) {
-      let newDate = addDays(startDay, i);
-      let timesheetData = this.getTimesheetFromDate(newDate);
-      this.selectedDays.push({
-        date: addDays(startDay, i),
-        status: timesheetData ? timesheetData.status : undefined,
-        timesheet: timesheetData ? timesheetData.timesheet : undefined
-      });
-    }
-  }
 
-  getHours(day: any, type: string) {
-    if (!day.timesheet) return 0;
-    return day.timesheet.reduce((acc: number, curr: any) => {
-      if (curr.engID[0] === type || type === 'ALL') return acc + curr.hours
-      return acc + 0
-    }, 0);
-  }
 
-  getTotalHours(type: string) {
-    return this.selectedDays.reduce((acc: number, curr: any) => {
-      if(!curr.timesheet) return acc + 0;
-      let tempAcc = 0;
-      curr.timesheet.forEach((t:any) => tempAcc += t.engID[0] === type || type === 'ALL' ? t.hours : 0)
-      return acc + tempAcc;
-    }, 0);
-  }
-
-  isInCurrentWeek(date: Date) {
-    for(let d of this.selectedDays) {
-      if(d.date.getDate() === date.getDate() && d.date.getMonth() == date.getMonth()) {
-        return true
-      };
-    }
-    return false;
-  }
-
-  getTimesheetFromDate(date: Date) {
-    for(let d of this.businessDaysData.days) {
-      const d1 = parse(d.date, "dd/MM/yyyy", new Date());
-      if(d1.getDate() === date.getDate() && d1.getMonth() == date.getMonth()) {
-        return d
-      };
-    }
-  }
-
+  ////////////////////////////// Functions for Calendar Section ////////////////////////////////
+  /**
+   * Build the calendar for a specific month
+   * @param {number} year - the year
+   * @param {number} month - the month index (0-based)
+   */
   getCalendarDays(year: number, month: number) {
     const start = startOfMonth(new Date(year, month));
     const end = endOfMonth(new Date(year, month));
@@ -159,6 +93,167 @@ export class AppComponent {
     }
     return days;
   }
+
+  /**
+   * Determines the styling/status of a date in the calendar UI
+   * @param {Date} date - the date to check
+   * @param {number} month - the current month
+   * @returns {string} - the CSS class for the date
+   */
+  getDateStatus(date: Date, month: number) {
+    let isToday = '';
+    if (date.getMonth() != month) {
+      return isToday + 'disabled'
+    }
+    if (date.getDay() === 6 || date.getDay() === 0) {
+      return isToday + 'nonworking'
+    }
+    if (date.getDate() === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+      isToday = 'today ';
+    }
+
+    if (this.isInSelectedWeek(date)) {
+      return isToday + 'current';
+    }
+
+    let dayFound = this.businessDaysData.days.filter((d: any) => {
+      const d1 = parse(d.date, "dd/MM/yyyy", new Date());
+      return  d1.getDate() === date.getDate() && d1.getMonth() == date.getMonth();
+    })[0];
+
+    return dayFound && dayFound.status ? isToday + dayFound.status.toLowerCase() : isToday + 'default'
+  }
+
+
+  /** 
+   * Update the selected data and trigger the update of the selected week
+   */
+  selectDate(date:Date) {
+    this.selectedDate = date;
+    this.updateSelectedWeek();
+  }
+
+  /**
+   * Updates the selected week based on the selected date
+   */
+  updateSelectedWeek() {
+    let startDay = addDays(this.selectedDate, -(this.selectedDate.getDay() + 1));
+    this.selectedWeek = [];
+    for (let i = 0; i < 7; i++) {
+      let newDate = addDays(startDay, i);
+      let timesheetData = this.getTimesheetFromDate(newDate);
+      this.selectedWeek.push({
+        date: addDays(startDay, i),
+        status: timesheetData ? timesheetData.status : undefined,
+        timesheet: timesheetData ? timesheetData.timesheet : undefined
+      });
+    }
+    this.updateTimesheetLines();
+  }
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  
+
+  
+
+  
+  //////////////////////////// Function for Summary Section ///////////////////////////////////
+  /**
+   * Calculates hours worked for a specific type of engagement on a given day.
+   * @param {any} day - the day containing timesheet entries
+   * @param {string} type - the type of engagement (e.g., 'E', 'I, 'A' 'ALL')
+   * @returns {number} - total hours worked for the specified type
+   */
+  getHours(day: any, type: string) {
+    if (!day.timesheet) return 0;
+    return day.timesheet.reduce((acc: number, curr: any) => {
+      if (curr.engID[0] === type || type === 'ALL') return acc + curr.hours
+      return acc + 0
+    }, 0);
+  }
+
+  /**
+   * Aggregates hours worked across the selected week for a specific type of engagement.
+   * @param {string} type - the type of engagement (e.g., 'E', 'I, 'A' 'ALL')
+   * @returns {number} - total hours worked for the specified type during the selected week
+   */
+  getTotalHours(type: string) {
+    return this.selectedWeek.reduce((acc: number, curr: any) => {
+      if(!curr.timesheet) return acc + 0;
+      let tempAcc = 0;
+      curr.timesheet.forEach((t:any) => tempAcc += t.engID[0] === type || type === 'ALL' ? t.hours : 0)
+      return acc + tempAcc;
+    }, 0);
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  /////////////////////////////Timesheet Section//////////////////////////////////////////////
+  /**
+   * Initializes timesheet with default values for 10 entries
+   */
+  initTimesheet() {
+    const blankToFill = -(this.timesheet.length-10);
+    for (let i = 0; i < blankToFill; i++) {
+      console.log(i, -(this.timesheet.length-10));
+      this.timesheet.push({ engID: undefined, actID: undefined, role: undefined, workLoc: undefined, sat: undefined, sun: undefined, mon: undefined, tue: undefined, wed: undefined, thu: undefined, fri: undefined });
+    }
+    console.log("TM=>", this.timesheet);
+  }
+
+  updateTimesheetLines() {
+    // Reducing the data array to the specified structure
+    const summary = this.selectedWeek.reduce((acc, item) => {
+      if (item.timesheet) {
+          item.timesheet.forEach(ts => {
+              const { engID, actID, role, workLoc, hours } = ts;
+              const dayOfWeek = this.getDayOfWeek(item.date);
+              const key = `${engID}_${actID}_${role}_${workLoc}`;
+              if (!acc[key]) {
+                  acc[key] = {engID,actID, role, workLoc, sat: '',sun: '',mon: '',tue: '',wed: '',thu: '',fri: ''};
+              }
+              acc[key][dayOfWeek] = hours;
+          });
+      }
+      return acc;
+    }, {});
+    // Converting the accumulated object into an array
+    this.timesheet = Object.values(summary);
+    this.initTimesheet();
+  }
+
+
+  ///////////////////////Helpers function/////////////////////////////////////
+  /**
+   * Checks if a date is within the selected week
+   * @param {Date} date - the date to check
+   * @returns {boolean}
+   */
+  isInSelectedWeek(date: Date) {
+    return this.selectedWeek.some(d => d.date.getDate() === date.getDate() && d.date.getMonth() === date.getMonth());
+  }
+
+  /**
+   * Retrieves timesheet data for a specific date
+   * @param {Date} date - the date to check
+   * @returns {any} - the timesheet data or undefined
+   */
+  getTimesheetFromDate(date: Date) {
+    for(let d of this.businessDaysData.days) {
+      const d1 = parse(d.date, "dd/MM/yyyy", new Date());
+      if(d1.getDate() === date.getDate() && d1.getMonth() == date.getMonth()) {
+        return d
+      };
+    }
+  }
+
+  // Helper function to get the day of week as string from date
+  getDayOfWeek(date: Date) {
+    const dayOfWeek = new Date(date).getUTCDay(); // Using UTC day to match your date string
+    console.log(date, dayOfWeek);
+    return ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'][dayOfWeek];
+  }
+
+  
 
 
 }
